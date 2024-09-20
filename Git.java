0a56b76fede4af04a67e378a0883eeede10de0e4
Giovanni Baldovino <gbaldovino1@hwemail.com>
2024-09-19 20:53:26 -0700

    Init Method Draft 1
    First draft of init method

--- Git.java
+++ Git.java
@@ -1,6 +1,30 @@
+import java.io.File;
+
 public class Git
 {
+    
     public static void main(String[] args) {
-        
+        init("\\\\wsl.localhost\\Ubuntu-20.04\\home\\HTCS\\git-project-Gio");
+    }
+    
+    public static void init(String path){
+        boolean exists = false;
+        File initFile = new File(path);
+        if(initFile.exists()){
+            exists = true;
+        }
+
+        initFile.mkdir("git");
+        File indexFile = new File("index");
+
+        if(indexFile.exists()){
+            exists = true;
+        }
+
+        if(exists){
+            System.out.println("Git Repository already exists");
+        } else {
+            System.out.println("Created Repo: " + path);
+        }
     }
 }
